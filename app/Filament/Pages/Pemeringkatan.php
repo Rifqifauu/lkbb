bd137e8dc853c40e45260ff3aee1978672b55495
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2025-08-28 13:45:44 +0700

    Pemeringkatan

--- app/Filament/Pages/Pemeringkatan.php
+++ app/Filament/Pages/Pemeringkatan.php
@@ -3,10 +3,27 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use App\Models\RekapNilai;
 
 class Pemeringkatan extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-trophy';
-
     protected static string $view = 'filament.pages.pemeringkatan';
+    protected static ?string $navigationLabel = 'Pemeringkatan';
+
+    // Ambil data untuk kategori utama
+    public function getUtama()
+    {
+        return RekapNilai::with('peserta')
+            ->orderByDesc('total_utama')
+            ->get();
+    }
+
+    // Ambil data untuk kategori umum
+    public function getUmum()
+    {
+        return RekapNilai::with('peserta')
+            ->orderByDesc('total_umum')
+            ->get();
+    }
 }
--- resources/views/filament/pages/pemeringkatan.blade.php
+++ resources/views/filament/pages/pemeringkatan.blade.php
@@ -1,3 +1,68 @@
-<x-filament-panels::page>
+<x-filament::page>
+    <div x-data="{ tab: 'utama' }" class="space-y-4">
+        <!-- Tabs -->
+        <div class="flex border-b border-gray-300">
+            <button
+                @click="tab = 'utama'"
+                :class="tab === 'utama' ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-500'"
+                class="px-4 py-2 font-medium"
+            >
+                Utama
+            </button>
+            <button
+                @click="tab = 'umum'"
+                :class="tab === 'umum' ? 'border-b-2 border-blue-500 text-blue-500' : 'text-gray-500'"
+                class="px-4 py-2 font-medium"
+            >
+                Umum
+            </button>
+        </div>
 
-</x-filament-panels::page>
+        <!-- Content -->
+        <div x-show="tab === 'utama'">
+            <table class="w-full table-auto border-collapse border border-gray-200 mt-4">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="border px-4 py-2">No</th>
+                        <th class="border px-4 py-2">Nama Peserta</th>
+                        <th class="border px-4 py-2">Tingkat</th>
+                        <th class="border px-4 py-2">Nilai Utama</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($this->getUtama() as $item)
+                        <tr class="hover:bg-gray-50">
+                            <td class="border px-4 py-2">{{ $loop->iteration }}</td>
+                            <td class="border px-4 py-2">{{ $item->peserta->nama }}</td>
+                            <td class="border px-4 py-2">{{ $item->peserta->tingkat }}</td>
+                            <td class="border px-4 py-2">{{ $item->total_utama }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div x-show="tab === 'umum'">
+            <table class="w-full table-auto border-collapse border border-gray-200 mt-4">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="border px-4 py-2">No</th>
+                        <th class="border px-4 py-2">Nama Peserta</th>
+                        <th class="border px-4 py-2">Tingkat</th>
+                        <th class="border px-4 py-2">Nilai Umum</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($this->getUmum() as $item)
+                        <tr class="hover:bg-gray-50">
+                            <td class="border px-4 py-2">{{ $loop->iteration }}</td>
+                            <td class="border px-4 py-2">{{ $item->peserta->nama }}</td>
+                            <td class="border px-4 py-2">{{ $item->peserta->tingkat }}</td>
+                            <td class="border px-4 py-2">{{ $item->total_umum }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-filament::page>
